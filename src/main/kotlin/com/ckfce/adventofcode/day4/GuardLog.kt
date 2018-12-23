3b7ad0010d4d0552cd18aa76407bd130aa2e0037
Ryan Pollock <rpollock@toasttab.com>
2018-12-22 21:49:49 -0500

    Day 4. Not in love with my solutions but they work

--- src/main/kotlin/com/ckfce/adventofcode/day4/GuardLog.kt
+++ src/main/kotlin/com/ckfce/adventofcode/day4/GuardLog.kt
@@ -1,9 +1,12 @@
 package com.ckfce.adventofcode.day4
 
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class GuardLog(private val log: List<String>) {
+    val sorted = sortLog()
+    val days = mutableListOf<SleepRecord>()
 
     fun sortLog(): List<String> {
         return log.asSequence()
@@ -15,4 +18,115 @@ class GuardLog(private val log: List<String>) {
         return LocalDateTime.parse(line.subSequence(1, 17),
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
     }
+
+    fun parse() {
+        var position = 0
+        while(position != log.size) {
+            position = parseDayRecord(position)
+        }
+    }
+
+    fun parseDayRecord(start: Int): Int {
+        // start guard
+        val guardId = extractGuard(sorted[start])
+        val date = extractDate(sorted[start]).toLocalDate()
+
+        var currentLine = start + 1
+        while (currentLine != sorted.size && notGuard(sorted[currentLine])) {
+            days.add(
+                SleepRecord(date, guardId,
+                    extractDate(sorted[currentLine]).minute,
+                    extractDate(sorted[currentLine + 1]).minute)
+            )
+            currentLine += 2
+        }
+        return currentLine
+    }
+
+    fun notGuard(line: String): Boolean {
+        return !line.contains("Guard")
+    }
+
+    fun extractGuard(e: String): Int {
+        return "Guard #([\\d]+) begins"
+            .toRegex()
+            .find(e)
+            ?.groups
+            ?.get(1)
+            ?.value!!.toInt()
+    }
+
+    fun findSleepiest(): Int {
+        val guardMinutesAsleep = mutableMapOf<Int, Int>()
+        for (sr in days) {
+            val c = guardMinutesAsleep[sr.guardId] ?: 0
+            guardMinutesAsleep[sr.guardId] = c + (sr.endMinute - sr.startMinute)
+        }
+
+        var sleepiestId = 0
+        var sleepiestMinutes = 0
+        for (guardAndMinute in guardMinutesAsleep.entries) {
+            if (guardAndMinute.value > sleepiestMinutes) {
+                sleepiestId = guardAndMinute.key
+                sleepiestMinutes = guardAndMinute.value
+            }
+        }
+
+        return sleepiestId
+    }
+
+    /**
+     * Find the minute that a guard is most likely to be asleep.
+     */
+    fun yawnyMinute(guardToCheckId: Int): Int {
+        val minuteToDaysSlept = mutableMapOf<Int, Int>()
+        for (sr in days) {
+            if (sr.guardId == guardToCheckId) {
+                for (i in sr.startMinute .. sr.endMinute) {
+                    if (minuteToDaysSlept[i] == null) {
+                        minuteToDaysSlept[i] = 1
+                    } else {
+                        minuteToDaysSlept[i] = minuteToDaysSlept[i]!!.plus( 1)
+                    }
+                }
+            }
+        }
+
+        var sleepiestMinute = 0
+        var daysAsleep = 0
+        for (md in minuteToDaysSlept.entries) {
+            if (md.value > daysAsleep) {
+                sleepiestMinute = md.key
+                daysAsleep = md.value
+            }
+        }
+
+        return sleepiestMinute
+    }
+
+    fun frequentMinute(): GuardMinute {
+        val guardMinuteCounts = mutableMapOf<GuardMinute, Int>()
+        for (sr in days) {
+            for (i in sr.startMinute .. sr.endMinute) {
+                val gm = GuardMinute(sr.guardId, i)
+                val c = guardMinuteCounts[gm] ?: 0
+                guardMinuteCounts[gm] = c + 1
+            }
+        }
+
+        var maxGuardMinute = GuardMinute(0, 0)
+        var maxCount = 0
+        for (gmc in guardMinuteCounts.entries) {
+            if (gmc.value > maxCount) {
+                maxGuardMinute = gmc.key
+                maxCount = gmc.value
+            }
+        }
+        return maxGuardMinute
+    }
 }
+
+data class SleepRecord(val date: LocalDate, val guardId: Int,
+    val startMinute: Int, val endMinute: Int)
+
+data class GuardMinute(val guardId: Int, val minute: Int)
--- src/test/kotlin/com/ckfce/adventofcode/day4/GuardLogTest.kt
+++ src/test/kotlin/com/ckfce/adventofcode/day4/GuardLogTest.kt
@@ -1,30 +1,87 @@
 package com.ckfce.adventofcode.day4
 
 import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class GuardLogTest {
 
     @Test
     fun testSortingLog() {
-        val l = GuardLog(testData())
+        val l = GuardLog(sampleData())
         val sorted = l.sortLog()
-        Assert.assertEquals("[1518-11-01 00:00]", sorted[0].substring(0, 18))
-        Assert.assertEquals("[1518-11-05 00:55]", sorted[sorted.size - 1].substring(0, 18))
+        assertEquals("[1518-11-01 00:00]", sorted[0].substring(0, 18))
+        assertEquals("[1518-11-05 00:55]", sorted[sorted.size - 1].substring(0, 18))
 
     }
 
     @Test
     fun testParseDate() {
-        val l = GuardLog(testData())
+        val l = GuardLog(sampleData())
         val d = l.extractDate( "[1518-11-05 00:03] Guard #99 begins shift")
 
-        Assert.assertEquals(1518, d.year)
-        Assert.assertEquals(5, d.dayOfMonth)
-        Assert.assertEquals(3, d.minute)
+        assertEquals(1518, d.year)
+        assertEquals(5, d.dayOfMonth)
+        assertEquals(3, d.minute)
     }
 
-    private fun testData(): List<String> {
+    @Test
+    fun testExtractGuard() {
+        val l = GuardLog(sampleData())
+        assertEquals(88, l.extractGuard("[1518-11-01 00:00] Guard #88 begins shift"))
+    }
+
+    @Test
+    fun testParse() {
+        val l = GuardLog(sampleData())
+        l.parse()
+        assertEquals(1, l.days[0].date.dayOfMonth)
+        assertEquals(5, l.days[0].startMinute)
+
+    }
+
+    @Test
+    fun testFindSleepy() {
+        val l = GuardLog(sampleData())
+        l.parse()
+        val guardId = l.findSleepiest()
+        assertEquals(10, guardId)
+    }
+
+    @Test
+    fun testYawnyMinute() {
+        val l = GuardLog(sampleData())
+        l.parse()
+        assertEquals(24, l.yawnyMinute(10))
+    }
+
+    @Test
+    fun solve1() {
+        val l = GuardLog(input())
+        l.parse()
+        val g = l.findSleepiest()
+        val m = l.yawnyMinute(g)
+        println(g * m)
+    }
+
+    @Test
+    fun testFrequentMinute() {
+        val l = GuardLog(sampleData())
+        l.parse()
+        val gm = l.frequentMinute()
+        assertEquals(99, gm.guardId)
+        assertEquals(45, gm.minute)
+    }
+
+    @Test
+    fun solve2() {
+        val l = GuardLog(input())
+        l.parse()
+        val gm = l.frequentMinute()
+        println(gm.guardId * gm.minute)
+    }
+
+    private fun sampleData(): List<String> {
         return listOf(
             "[1518-11-04 00:36] falls asleep",
             "[1518-11-04 00:46] wakes up",
@@ -44,5 +101,10 @@ class GuardLogTest {
             "[1518-11-01 00:05] falls asleep",
             "[1518-11-01 00:25] wakes up"
         )
+    }
+
+    private fun input(): List<String> {
+        val i = this.javaClass.getResource("input.txt").readText(Charsets.UTF_8)
+        return i.split("\n").filter { it.isNotBlank() }
     }
 }
